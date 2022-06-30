647d2e4810d92358b05104a3160bb2f48947f772
EdgarKurashov <edgarkurasov@gmail.com>
2022-07-01 02:50:52 +0300

    2

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -39,6 +39,16 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\setLanguage::class,
         ],
 
+        'admin' => [
+            \App\Http\Middleware\EncryptCookies::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \App\Http\Middleware\VerifyCsrfToken::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\setLanguage::class,
+        ],
+
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:api',
